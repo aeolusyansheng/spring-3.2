79c92bfff2dc2b2d1c64dcc1677d5bc6d88836cf
yansheng <aeolusyansheng@gmail.com>
2017-12-07 22:07:25 +0900

    2017/12/7 from mac

--- src/com/yansheng/beans/BeansException.java
+++ src/com/yansheng/beans/BeansException.java
@@ -1,5 +1,12 @@
 package com.yansheng.beans;
 
-public abstract class BeansException {
+import org.springframework.core.NestedRuntimeException;
 
+public abstract class BeansException extends NestedRuntimeException {
+
+	public BeansException(String msg) {
+		super(msg);
+	}
+	
+	//TODO
 }
